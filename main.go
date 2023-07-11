c58b655d80fabcac356cea08d67ffe1cd15058a9
AmirDaraby <amir2002.d@gmail.com>
2023-07-12 01:41:45 +0330

    drawing circles on mouse left click in random locations

--- main.go
+++ main.go
@@ -1,22 +1,50 @@
 package main
 
 import (
+	"fmt"
+	"math/rand"
+
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
+type Ball struct {
+	x, y   int
+	radius float32
+}
+
+const screenHeight int32 = 450
+const screenWidth int32 = 800
+const minX, maxX int = 70, 750
+const minY, maxY int = 70, 400
+
 func main() {
 
-	rl.InitWindow(800, 450, "this is a test")
+	rl.InitWindow(screenWidth, screenHeight, "SHOOT CIRCLE")
+
+	defer println("shutting down")
 	defer rl.CloseWindow()
+
 	rl.SetTargetFPS(30)
+
+	ball := Ball{}
+
+	initBall(&ball)
 
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
 
-		rl.ClearBackground(rl.RayWhite)
+		rl.ClearBackground(rl.Black)
 
-		rl.DrawText("This is a test, thanks for watching", 190, 200, 20, rl.LightGray)
+		rl.DrawCircle(int32(ball.x), int32(ball.y), ball.radius, rl.Maroon)
 
+		if rl.IsMouseButtonPressed(rl.MouseLeftButton) {
+			fmt.Println(&ball)
+			initBall(&ball)
+		}
 		rl.EndDrawing()
 	}
+}
+
+func initBall(b *Ball) {
+	*b = Ball{x: rand.Intn(maxX-minX) + minX, y: rand.Intn(maxY-minY) + minY, radius: 30}
 }
